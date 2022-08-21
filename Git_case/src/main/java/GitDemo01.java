a822f52dd6f8ba2d330f00509712e257f6740cc7
Pang <1780681114@qq.com>
2022-08-21 15:58:05 +0800

    push commit

--- Git_case/src/main/java/GitDemo01.java
+++ Git_case/src/main/java/GitDemo01.java
@@ -4,5 +4,6 @@ public class GitDemo01 {
         System.out.println("hello git!!");
         System.out.println("hello git!!2");
         System.out.println("hello git! hello hot fix");
+        System.out.println("push commit");
     }
 }
